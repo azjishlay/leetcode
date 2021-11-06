64b6bbeb5e57ebd19049d56d6c3e74f08b4683e5
azjishlay <16637558+azjishlay@users.noreply.github.com>
2021-11-06 19:27:17 -0400

    Adds files for session #1

--- s01-two-sum/three_sum.py
+++ s01-two-sum/three_sum.py
@@ -3,11 +3,14 @@ def three_sum(arr, target):
 
     if arr and len(arr) >= 3:
         arr.sort()
-        for i in range(len(arr)):
+
+        for i in range(len(arr) - 2):
             l = i + 1
             r = len(arr) - 1
+            print(i, l, r)
 
             while l < r:
+                # print("start ", i, l, r)
                 sum = arr[i] + arr[l] + arr[r]
 
                 if sum < target:
@@ -16,8 +19,11 @@ def three_sum(arr, target):
                     r -= 1
                 else:
                     a.append([arr[i], arr[l], arr[r]])
-                    l += 1
-                    r -= 1
+                    # l += 1
+                    # r -= 1
+
+                    return a
+
     return a
 
 arr1 = [-1, 0, 1, 2, -1, -4]
@@ -27,9 +33,12 @@ arr2 = [-1, 0]
 arr3 = []
 arr4 = [0, 0, 0]
 
-arr5 = [0, 0, -1, 1, 2, -2, 3, 5, -3]
+arr5 = [0, 0, -1, 1, 2, -2, 3, 5, -3, 8, 4, 2, -9, -3]
 a5 = [[-3, -2, 5], [-3, 0, 3], [-3, 1, 2], [-2, -1, 3], [-2, 0, 2], [-1, 0, 1]]
 
-x = 0
+arr6 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+arr7 = [-1, -2, -3, -4, -5, -6, -7, -8, 9]
 
-print(three_sum(arr5, x))
+x = 18
+
+print(three_sum(arr6, x))
